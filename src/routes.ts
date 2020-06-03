ccb13938fb903f2603f2d7fa591d8be5450a9c29
lucascprazeres <olucascostadp@gmail.com>
2020-06-03 12:07:26 -0300

    created the ItemsController & its "index" method

--- src/routes.ts
+++ src/routes.ts
@@ -1,24 +1,13 @@
 import express from 'express';
-import knex from './database/connection';
 
 import PointsController from './Controllers/PointsController';
+import ItemsController from './Controllers/ItemsController';
 
 const routes = express.Router();
 const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', async (request, response) => {
-  const items = await knex('*').from('items');
-
-  const serializedItems = items.map(item => {
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
-    }
-  })
-
-  return response.json(serializedItems);
-});
+routes.get('/items', itemsController.index);
 
 routes.post('/points', pointsController.create);
 
