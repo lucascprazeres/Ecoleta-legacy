f60ad9aaeeb3368920d6a9a4c5139638b46feafd
lucascprazeres <olucascostadp@gmail.com>
2020-06-03 11:38:19 -0300

    point & point_items insertion

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { request, response } from 'express';
 import knex from './database/connection';
 
 const routes = express.Router();
@@ -8,12 +8,51 @@ routes.get('/items', async (request, response) => {
 
   const serializedItems = items.map(item => {
     return {
+      id: item.id,
       title: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`,
     }
   })
 
-  return response.json(serializedItems); 
+  return response.json(serializedItems);
 });
+
+routes.post('/points', async (request, response) => {
+  const {
+    name,
+    image,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = request.body;
+
+  const insertedIds = await knex('points').insert({
+    name,
+    image: 'fake-image',
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id,
+    }
+  })
+
+  await knex('point_items').insert(pointItems);
+
+  return response.json({ success: true });
+})
 
 export default routes;
