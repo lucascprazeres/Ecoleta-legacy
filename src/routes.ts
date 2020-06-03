933f436bab1178572e84ff43590f801664579ac4
lucascprazeres <olucascostadp@gmail.com>
2020-06-03 11:03:57 -0300

    listing images on database

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: "placeholder" });
+routes.get('/items', async (request, response) => {
+  const items = await knex('*').from('items');
+
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    }
+  })
+
+  return response.json(serializedItems); 
 });
 
 export default routes;
