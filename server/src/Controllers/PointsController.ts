4d09b55d5a6424d2db2210a2c6ff8adcc1cc6022
lucascprazeres <olucascostadp@gmail.com>
2020-06-06 15:16:16 -0300

    server - added image upload

--- server/src/Controllers/PointsController.ts
+++ server/src/Controllers/PointsController.ts
@@ -17,7 +17,14 @@ class PointsController {
       .distinct()
       .select('points.*');
 
-    return response.json(points);
+    const serializedPoints = points.map(point => {
+      return {
+        ...point,
+        image_url: `http://192.168.0.26:3333/uploads/${point.image}`,
+      }
+    })
+
+    return response.json(serializedPoints);
   }
   async show(request: Request, response: Response) {
     const { id } = request.params;
@@ -27,6 +34,11 @@ class PointsController {
     if (!point) {
       return response.status(400).json({ message: 'Point not found.' })
     }
+
+    const serializedPoint = {
+        ...point,
+        image_url: `http://192.168.0.26:3333/uploads/${point.image}`,
+      }
 
     /*
       SELECT * FROM items
@@ -39,7 +51,7 @@ class PointsController {
       .where('point_items.point_id', id)
       .select('items.title');
 
-    return response.json({ point, items });
+    return response.json({ point: serializedPoint, items });
   }
 
   async create(request: Request, response: Response) {
@@ -54,12 +66,12 @@ class PointsController {
       uf,
       items
     } = request.body;
-  
+
     const trx = await knex.transaction();
 
     const point = {
       name,
-      image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+      image: request.file.filename,
       email,
       whatsapp,
       latitude,
@@ -67,22 +79,25 @@ class PointsController {
       city,
       uf,
     }
-  
+
     const insertedIds = await trx('points').insert(point);
-  
+
     const point_id = insertedIds[0];
-  
-    const pointItems = items.map((item_id: number) => {
-      return {
-        item_id,
-        point_id,
-      }
-    })
-  
+
+    const pointItems = items
+      .split(',')
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
+        return {
+          item_id,
+          point_id,
+        }
+      })
+
     await trx('point_items').insert(pointItems);
 
     await trx.commit();
-  
+
     return response.json({
       point_id,
       ...point
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,14 +3,20 @@ import express from 'express';
 import PointsController from './Controllers/PointsController';
 import ItemsController from './Controllers/ItemsController';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 const routes = express.Router();
+
+const upload = multer(multerConfig);
+
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
-
-routes.post('/points', pointsController.create);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
+
+routes.post('/points', upload.single('image'), pointsController.create);
 
 export default routes;
