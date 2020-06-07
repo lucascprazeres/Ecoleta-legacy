51c843873be798062786d0abca4237367c665e81
lucascprazeres <olucascostadp@gmail.com>
2020-06-06 23:05:01 -0300

    added serverside validation

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import path from 'path';
 import routes from './routes';
 
+import { errors } from 'celebrate';
+
 const app = express();
 
 app.use(cors());
@@ -10,6 +12,8 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333, () => {
   console.log('Server avaliable on http://localhost:3333/')
