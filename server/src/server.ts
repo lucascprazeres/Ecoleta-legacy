8d2e6b8e69ffbb1a6ce7c6018d1bec1f6ca21c34
Lucas Prazeres <olucascostadp@gmail.com>
2021-05-08 19:44:34 -0300

    docs(api): started documenting the routes with swagger

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,13 +3,16 @@ import cors from "cors";
 import express from "express";
 import ip from "ip";
 import path from "path";
+import * as swaggerUI from "swagger-ui-express";
 
 import routes from "./routes";
+import swaggerDocs from "./swagger.json";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use(routes);
 
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
